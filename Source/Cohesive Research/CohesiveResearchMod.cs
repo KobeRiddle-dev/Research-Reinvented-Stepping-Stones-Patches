85b47c7e083868834222264679566193050b5be5
Kobe Riddle <mail@koberiddle.dev>
2026-07-25 15:32:03 -0600

    Initialized C# project.

---
+++ Source/Cohesive Research/CohesiveResearchMod.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace Cohesive_Research;
+
+public class CohesiveResearchMod : Mod
+{
+    public CohesiveResearchMod(ModContentPack content) : base(content)
+    {
+        
+    }
+}
+
